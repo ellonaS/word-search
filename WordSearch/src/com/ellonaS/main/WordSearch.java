8b3ca4ec3f8e3cd2c58dfad2367f325496a7577d
ellonaS <ellonasharafieva@gmail.com>
2020-05-05 16:02:03 -0400

    add test method searchTheGridForwardTest

--- WordSearch/src/com/ellonaS/main/WordSearch.java
+++ WordSearch/src/com/ellonaS/main/WordSearch.java
@@ -8,37 +8,51 @@ public class WordSearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WordSearch app = new WordSearch();
+		app.displayGrid();
 
 	}
-	
-	public List <char[]> generateRandomLetterGrid(int gridSize) {
-		
-		List <char[]> grid = new ArrayList <char[]>();
-		
-		for(int i = 0; i < gridSize; i++) {
+
+	public List<char[]> generateRandomLetterGrid(int gridSize) {
+
+		List<char[]> grid = new ArrayList<char[]>();
+
+		for (int i = 0; i < gridSize; i++) {
 			char[] letters = generateRandomLetterArray(gridSize);
 			grid.add(letters);
 		}
-		
+
 		return grid;
 	}
-	
+
 	private char[] generateRandomLetterArray(int arrayLength) {
-		//create an array of all alphabet letters
-		char[] alphabet = new char[26];
+		// create a list of all alphabet letters
+		List <Character> alphabet = new ArrayList<Character>();
 		for (char c = 'A'; c <= 'Z'; c++) {
-			for (int i = 0; i < alphabet.length; i++) {
-				alphabet[i] = c;
-			}
+			alphabet.add(c);
 		}
-		//create a new char array, loop through and assign each char to random letter 
-		//from alphabet array
-		char []randomLetters = new char [arrayLength];
+		// create a new char array, loop through and assign each char to random letter
+		// from alphabet list
+		char[] randomLetters = new char[arrayLength];
 		Random random = new Random();
-		for (int i = 0; i < randomLetters.length; i++ ) {
-			randomLetters[i] = alphabet [random.nextInt(alphabet.length)];
+		for (int i = 0; i < randomLetters.length; i++) {
+			randomLetters[i] = alphabet.get(random.nextInt(alphabet.size()));
+
 		}
 		return randomLetters;
+	}
+
+	public void displayGrid() {
+		List<char[]> grid = generateRandomLetterGrid(15);
+		for (int i = 0; i < grid.size(); i++) {
+			String str = new String(grid.get(i));
+			System.out.println(str);
+		}
+	}
+	
+	public int searchTheGridForward(List <char[]> grid, String word) {
+	
+		return 0;
 	}
 
 }
--- WordSearch/src/com/ellonaS/test/WordSearchTest.java
+++ WordSearch/src/com/ellonaS/test/WordSearchTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
@@ -13,42 +14,52 @@ import com.ellonaS.main.WordSearch;
 
 import junit.framework.Assert;
 
-public class WordSearchTest{
-	
+public class WordSearchTest {
+
 	WordSearch wordSearch;
-	List <char[]> grid;
+	List<char[]> grid;
 	int gridSize;
-	
+
 	@Before
 	public void setup() {
 		wordSearch = new WordSearch();
 		gridSize = 15;
 		grid = wordSearch.generateRandomLetterGrid(gridSize);
-		
+
 	}
-	
+
 	@Test
 	public void gridSizeTest() {
 		assertNotNull(grid);
-		assertEquals (gridSize, grid.size());
+		assertEquals(gridSize, grid.size());
 		for (int i = 0; i < gridSize; i++) {
-			assertEquals (gridSize, grid.get(i).length);
+			assertEquals(gridSize, grid.get(i).length);
 		}
-		
+
 	}
+
 	@Test
 	public void gridConsistsOfLettersTest() {
 		for (int i = 0; i < gridSize; i++) {
 			char[] letters = grid.get(i);
-			
-			for (char c :letters) {
+
+			for (char c : letters) {
 				assertTrue(Character.isLetter(c));
 			}
 		}
-		
+
 	}
 
-
-
+	@Test
+	public void searchTheGridForwardTest() {
+		List<char[]> grid = new ArrayList<char[]>();
+		char[] letters1 = { 'B', 'O', 'O', 'K', 'S', 'S', 'B', 'O', 'O', 'K'};
+		grid.add(letters1);
+		char[] letters2 = {'S', 'B', 'O', 'O', 'K', 'S', 'B', 'O', 'O', 'K'};
+		grid.add(letters2);
+		char[] letters3 = {'S', 'A', 'B', 'O', 'K', 'S', 'A', 'B', 'O', 'K'};
+		grid.add(letters3);
+		assertEquals(4, wordSearch.searchTheGridForward(grid, "BOOK"));
+	}
 
 }
