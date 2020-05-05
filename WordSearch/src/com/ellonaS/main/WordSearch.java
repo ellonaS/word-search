eb72ce2a1700c86cf7e2c8b887ea93de44db72f8
ellonaS <ellonasharafieva@gmail.com>
2020-05-05 14:25:13 -0400

    add passing code fortest method gridConsistsOfLettersTest

--- WordSearch/src/com/ellonaS/main/WordSearch.java
+++ WordSearch/src/com/ellonaS/main/WordSearch.java
@@ -2,6 +2,7 @@ package com.ellonaS.main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class WordSearch {
 
@@ -15,11 +16,29 @@ public class WordSearch {
 		List <char[]> grid = new ArrayList <char[]>();
 		
 		for(int i = 0; i < gridSize; i++) {
-			char[] letters = new char[gridSize];
+			char[] letters = generateRandomLetterArray(gridSize);
 			grid.add(letters);
 		}
 		
 		return grid;
+	}
+	
+	private char[] generateRandomLetterArray(int arrayLength) {
+		//create an array of all alphabet letters
+		char[] alphabet = new char[26];
+		for (char c = 'A'; c <= 'Z'; c++) {
+			for (int i = 0; i < alphabet.length; i++) {
+				alphabet[i] = c;
+			}
+		}
+		//create a new char array, loop through and assign each char to random letter 
+		//from alphabet array
+		char []randomLetters = new char [arrayLength];
+		Random random = new Random();
+		for (int i = 0; i < randomLetters.length; i++ ) {
+			randomLetters[i] = alphabet [random.nextInt(alphabet.length)];
+		}
+		return randomLetters;
 	}
 
 }
